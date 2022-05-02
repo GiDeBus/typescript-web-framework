50ef67b752c40cde89ed4394ee632aa29ef905d7
Gil <gidelbusto@gmail.com>
2022-05-02 12:56:50 +0700

    ADD: refactor get and on methods to be arrow funcs

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -2,11 +2,11 @@ export default class Attributes<T> {
 
   constructor(private data: T) {}
 
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
   }
 
-  set(update: T): void {
+  set = (update: T): void => {
     Object.assign(this.data, update); 
   }
 }
