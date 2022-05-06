7241fbf522c3ea6ddb0168e20354c8fbdd07efda
Gil <gidelbusto@gmail.com>
2022-05-06 22:06:39 +0700

    ADD: getAll( ) method to Attributes class

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,12 +1,15 @@
 export default class Attributes<T> { 
-
   constructor(private data: T) {}
 
   get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
-  }
+  };
 
   set = (update: T): void => {
     Object.assign(this.data, update); 
+  }
+
+  getAll(): T { 
+    return this.data;
   }
 }
